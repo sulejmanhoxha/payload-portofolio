541d1a0e363b12591c43d600caeb37a89a7bc70a
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2025-02-03 00:53:07 +0100

    update motion

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { Dispatch, SetStateAction, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
